edc1d3541f1aa560f2053d9c6ec4a14181dd654e
KRofLife <05410kira@gmail.com>
2021-10-13 15:49:10 +0000

    【コメント機能の非同期通信化】

--- app/controllers/square_comments_controller.rb
+++ app/controllers/square_comments_controller.rb
@@ -1,16 +1,21 @@
 class SquareCommentsController < ApplicationController
   def create
-    square = Square.find(params[:square_id])
+    @square = Square.find(params[:square_id])
     comment = current_user.square_comments.new(square_comment_params)
-    comment.square_id = square.id
+    comment.square_id = @square.id
     if comment.save
+      # redirect_to square_path(square)
+    else
       redirect_to square_path(square)
     end
   end
 
   def destroy
-    SquareComment.find_by(id: params[:id]).destroy
-    redirect_to square_path(params[:square_id])
+    @square = Square.find(params[:square_id])
+    square_comment = @square.square_comments.find(params[:id])
+    square_comment.destroy
+    # SquareComment.find_by(id: params[:id]).destroy
+    # redirect_to square_path(params[:square_id])
   end
 
   private
