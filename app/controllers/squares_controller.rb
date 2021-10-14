9b3f6cd4dfd4bd9560d63db3e36757a206cf766c
KRofLife <05410kira@gmail.com>
2021-10-14 05:20:06 +0000

    【Square編集ページ修正・seedファイル追加】

--- app/controllers/squares_controller.rb
+++ app/controllers/squares_controller.rb
@@ -15,7 +15,7 @@ class SquaresController < ApplicationController
   end
 
   def index
-    @squares = Square.page(params[:page]).reverse_order
+    @squares = Square.where(is_released: "true").page(params[:page]).reverse_order
   end
 
   def show
@@ -24,15 +24,25 @@ class SquaresController < ApplicationController
     @square_comment = SquareComment.new
   end
 
+  def update
+    @square = Square.find(params[:id])
+    @square.update(square_params)
+    redirect_to square_path(@square.id)
+  end
+
   def destroy
     @square = Square.find(params[:id])
     @square.destroy
     redirect_to squares_path
   end
 
+  def edit
+    @square = Square.find(params[:id])
+  end
+
   private
 
   def square_params
-    params.require(:square).permit(:image, :title, :description, :user_id, :square_id, :category_id)
+    params.require(:square).permit(:image, :title, :description, :user_id, :square_id, :category_id, :is_released)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -44,82 +44,82 @@ Square.create!(
   user_id: 1,
   category_id: 3,
   is_completed: false,
-  is_released: false,
+  is_released: true,
   image: File.open("./app/assets/images/square1.jpg")
   )
 
 Square.create!(
   title: "ロールスロイスに乗る",
   description: "テスト２",
-  user_id: 1,
+  user_id: 2,
   category_id: 1,
   is_completed: false,
-  is_released: false,
+  is_released: true,
   image: File.open("./app/assets/images/square2.jpg")
   )
 
 Square.create!(
   title: "バンジージャンプする",
   description: "テスト3",
-  user_id: 1,
+  user_id: 3,
   category_id: 3,
   is_completed: false,
-  is_released: false,
+  is_released: true,
   image: File.open("./app/assets/images/square3.jpg")
   )
 Square.create!(
   title: "世界一周する",
   description: "テスト4",
-  user_id: 1,
+  user_id: 4,
   category_id: 3,
   is_completed: false,
-  is_released: false,
+  is_released: true,
   image: File.open("./app/assets/images/square4.jpg")
   )
 Square.create!(
   title: "家族と幸せに暮らす",
   description: "テスト5",
-  user_id: 1,
+  user_id: 5,
   category_id: 4,
   is_completed: false,
-  is_released: false,
+  is_released: true,
   image: File.open("./app/assets/images/square5.jpg")
   )
 
 Square.create!(
   title: "健康に暮らす",
   description: "テスト6",
-  user_id: 1,
+  user_id: 6,
   category_id: 6,
   is_completed: false,
-  is_released: false,
+  is_released: true,
   image: File.open("./app/assets/images/square6.jpg")
   )
 Square.create!(
   title: "エンジニアになる",
   description: "テスト7",
-  user_id: 1,
+  user_id: 7,
   category_id: 7,
   is_completed: false,
-  is_released: false,
+  is_released: true,
   image: File.open("./app/assets/images/square7.jpg")
   )
 Square.create!(
   title: "「ありがとう」を当たり前にする",
   description: "テスト8",
-  user_id: 1,
+  user_id: 8,
   category_id: 7,
   is_completed: false,
-  is_released: false,
+  is_released: true,
   image: File.open("./app/assets/images/square8.jpg")
   )
 Square.create!(
   title: "仲間と共に切磋琢磨する",
   description: "テスト9",
-  user_id: 1,
+  user_id: 9,
   category_id: 5,
   is_completed: false,
-  is_released: false,
+  is_released: true,
   image: File.open("./app/assets/images/square9.jpg")
   )
 
@@ -130,3 +130,15 @@ User.create!(
   password: "testtest",
   profile_image: File.open("./app/assets/images/sample.jpg")
   )
+
+require 'faker'
+
+15.times do |n|
+  sample_name = Gimei.kanji
+  sample_email = Faker::Internet.email
+  User.create!(
+    name:         sample_name,
+    email:        sample_email,
+    password: "testtest"
+  )
+end
